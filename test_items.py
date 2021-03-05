6689afaf25d23a27819bf5d0d501c7722e9ab258
UseGoogle <tigrabeg@gmail.com>
2021-03-05 17:36:43 +0200

    Fix define naming

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_check_add_to_card(browser):
+def test_check_add_to_card_displayed(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     button = browser.find_element_by_class_name('btn-add-to-basket')
     assert button, 'button is displayed'
